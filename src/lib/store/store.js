882e6b3d38d1c7845d4ac197a7064c826d6620d6
Michal Wawryszuk <mwawryszuk@evojam.com>
2018-01-10 12:22:36 +0100

    ADD devtools for redux

--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -3,4 +3,8 @@ import {createStore} from 'redux'
 import {reducers} from './reducers'
 import {initialState} from './initial-state'
 
-export const store = createStore(reducers, initialState)
+export const store = createStore(
+  reducers,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // TODO: remove on production
+)

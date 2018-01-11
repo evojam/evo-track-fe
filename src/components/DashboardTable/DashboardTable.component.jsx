2e09a97988c96ef357479b208efc146971895f1a
Bartek <bmaciejiczek@evojam.com>
2018-01-11 11:00:45 +0100

    Styling 1/2

--- src/components/DashboardTable/DashboardTable.component.jsx
+++ src/components/DashboardTable/DashboardTable.component.jsx
@@ -1,11 +1,65 @@
 import React, { Component } from "react"
+import PropTypes from 'prop-types'
+
+import './DashboardTable.css'
 
 export class DashboardTable extends Component {
+  constructor(props) {
+    super(props)
+
+    this.renderBodyRows = this.renderBodyRows.bind(this)
+    this.renderHeaderCells = this.renderHeaderCells.bind(this)
+  }
+
   render() {
+    const { data } = this.props
+
     return (
-      <div>
-        Dashboard table!
+      <div className="table-responsive">
+        <table className="table table-bordered dashboard-table">
+          <thead className="thead-inverse">
+            <th>Name</th>
+            <th>∑</th>
+            {this.renderHeaderCells()}
+          </thead>
+          <tbody>
+            {this.renderBodyRows()}
+          </tbody>
+        </table>
       </div>
     )
   }
+
+  renderHeaderCells() {
+    const { data } = this.props
+
+    if (data[0] !== undefined) {
+      return data[0].data.map(day => (
+        <th className="day-th">{day.date}</th>
+      ))
+    }
+  }
+
+  renderBodyRows() {
+    const { data } = this.props
+
+    return data.map(user => {
+      const renderDaysCells = days =>
+        days.map(day => (
+          <td className="day-td">{day.minutes}</td>
+        ))
+
+      return (
+        <tr>
+          <td>{user.name}</td>
+          <td></td>
+          {renderDaysCells(user.data)}
+        </tr>
+      )
+    })
+  }
+}
+
+DashboardTable.propTypes = {
+  data: PropTypes.object,
 }

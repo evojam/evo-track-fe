63467b5e5db15ecadf367d9c63429d175e128e41
Antoni Karolewski <akarolewski@evojam.com>
2018-01-12 11:23:06 +0100

    popover-style

--- src/components/Details/Detail.js
+++ src/components/Details/Detail.js
@@ -3,14 +3,16 @@ import { Popover, PopoverHeader, PopoverBody } from 'reactstrap'
 
 import { changeMinutesToString } from 'helpers'
 
+import './Detail.css'
+
 export class Details extends React.Component{
   render() {
     return (
       <div>
-        <Popover placement="bottom" isOpen={this.props.open} target={`Popover${this.props.ind}`} toggle={this.props.toggle}>
+        <Popover className="detail-popover" placement="bottom" isOpen={this.props.open} target={`Popover${this.props.ind}`} toggle={this.props.toggle}>
           <PopoverHeader>{this.props.date}</PopoverHeader>
           <PopoverBody>
-            <span>{this.props.user}</span>
+            <span className="details-name">{this.props.user}</span>
             {this.props.issues.map((issue, index) => (
               <p key={index}>
                 <a href={`https://evojam.atlassian.net/browse/${issue.key}`} target="_blank">{issue.key}</a>
@@ -23,4 +25,3 @@ export class Details extends React.Component{
     )
   }
 }
-
